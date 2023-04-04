150f5af525de0b7d4c44710deb591353351b3120
BrianSammit <briansammit.cruz@gmail.com>
2023-04-03 19:54:16 -0500

    Add use case config

--- applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
@@ -1,14 +1,41 @@
 package co.com.bancolombia.config;
 
+import co.com.bancolombia.model.product.gateways.ProductGateway;
+import co.com.bancolombia.usecase.saveproduct.SaveProductUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = "co.com.bancolombia.usecase",
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
-        },
-        useDefaultFilters = false)
+@ComponentScan(basePackages = "co.com.bancolombia.usecase")
+//        includeFilters = {
+//                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+//        },
+//        useDefaultFilters = false)
 public class UseCasesConfig {
+//        @Bean
+//        public GetAllProductsUseCase getAllProductsUseCase(ProductRepository gateway){
+//                return new GetAllProductsUseCase(gateway);
+//        }
+
+//        @Bean
+//        public GetProductByIdUseCase getProductByIDUseCase(ProductRepository gateway){
+//                return new GetProductByIdUseCase(gateway);
+//        }
+
+        @Bean
+        public SaveProductUseCase saveProductUseCaseUseCase(ProductGateway gateway){
+                return new SaveProductUseCase(gateway);
+        }
+
+//        @Bean
+//        public UpdateProductUseCase updateProductUseCase(ProductRepository gateway){
+//                return new UpdateProductUseCase(gateway);
+//        }
+
+//        @Bean
+//        public DeleteProductUseCase deleteProductUseCase(ProductRepository gateway){
+//                return new DeleteProductUseCase(gateway);
+//        }
 }
